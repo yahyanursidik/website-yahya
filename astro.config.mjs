bbe3de176dc59abcf8b9d6f506784d22568b525d
yahyanursidik <yahyanursidik@gmail.com>
2026-06-02 06:42:19 +0700

    fix(keystatic): add site to astro config to fix oauth callback

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://yahyanursidik.my.id',
   integrations: [
     react(),
     tailwind(),
